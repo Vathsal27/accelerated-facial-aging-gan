b4c4ab2cc3dbd3c30002624b8416ad2be2b6c513
Vathsal27 <vathsal.tammewar27@gmail.com>
2025-04-22 02:04:50 +0530

    fine tuned code

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,32 +1,140 @@
 import tensorflow as tf
 from tensorflow.keras import layers
+from tensorflow.keras import initializers
 
 class StarDiscriminator(tf.keras.Model):
     def __init__(self, config):
         super().__init__()
         self.config = config
         
-        # Shared layers
+        # Kernel initializer
+        kernel_init = initializers.RandomNormal(mean=0.0, stddev=0.02)
+        
+        # Shared layers with spectral normalization
         self.shared = tf.keras.Sequential([
-            layers.Conv2D(64, 4, strides=2, padding='same'),
+            layers.Conv2D(64, 4, strides=2, padding='same',
+                         kernel_initializer=kernel_init,
+                         use_bias=False),
             layers.LeakyReLU(0.2),
             
-            layers.Conv2D(128, 4, strides=2, padding='same'),
+            SpectralNormalization(
+                layers.Conv2D(128, 4, strides=2, padding='same',
+                             kernel_initializer=kernel_init,
+                             use_bias=False)
+            ),
             layers.LeakyReLU(0.2),
             
-            layers.Conv2D(256, 4, strides=2, padding='same'),
+            SpectralNormalization(
+                layers.Conv2D(256, 4, strides=2, padding='same',
+                             kernel_initializer=kernel_init,
+                             use_bias=False)
+            ),
             layers.LeakyReLU(0.2),
             
-            layers.Conv2D(512, 4, strides=2, padding='same'),
+            SpectralNormalization(
+                layers.Conv2D(512, 4, strides=2, padding='same',
+                             kernel_initializer=kernel_init,
+                             use_bias=False)
+            ),
             layers.LeakyReLU(0.2)
         ])
         
         # Output layers
-        self.src = layers.Conv2D(1, 3, padding='same')
-        self.cls = layers.Dense(config.NUM_DOMAINS)
+        self.src = SpectralNormalization(
+            layers.Conv2D(1, 3, padding='same',
+                         kernel_initializer=kernel_init)
+        )
         
+        self.cls = SpectralNormalization(
+            layers.Dense(config.NUM_DOMAINS,
+                        kernel_initializer=kernel_init)
+        )
+        
+        # Minibatch discrimination (optional)
+        self.minibatch_layer = MinibatchDiscrimination(
+            num_kernels=100, 
+            dim_per_kernel=5
+        )
+
     def call(self, x, training=None):
         features = self.shared(x)
+        
+        # Add minibatch discrimination features
+        mb_features = self.minibatch_layer(features)
+        features = tf.concat([features, mb_features], axis=-1)
+        
         validity = self.src(features)
         label = self.cls(layers.Flatten()(features))
+        
         return validity, label
+
+
+# Helper Classes ==============================================
+
+class SpectralNormalization(layers.Wrapper):
+    """Spectral normalization layer wrapper for GAN stability"""
+    def __init__(self, layer, iteration=1, eps=1e-12, **kwargs):
+        super().__init__(layer, **kwargs)
+        self.iteration = iteration
+        self.eps = eps
+        
+    def build(self, input_shape):
+        self.layer.build(input_shape)
+        self.w = self.layer.kernel
+        self.w_shape = self.w.shape.as_list()
+        self.u = self.add_weight(
+            shape=(1, self.w_shape[-1]),
+            initializer='random_normal',
+            trainable=False,
+            name='sn_u',
+            dtype=tf.float32
+        )
+        
+    def call(self, inputs):
+        self._compute_weights()
+        output = self.layer(inputs)
+        return output
+        
+    def _compute_weights(self):
+        w_reshaped = tf.reshape(self.w, [-1, self.w_shape[-1]])
+        u = self.u
+        
+        for _ in range(self.iteration):
+            v = tf.math.l2_normalize(tf.matmul(u, w_reshaped, transpose_b=True))
+            u = tf.math.l2_normalize(tf.matmul(v, w_reshaped))
+            
+        sigma = tf.matmul(tf.matmul(v, w_reshaped), u, transpose_b=True)
+        w_bar = self.w / sigma
+        
+        self.layer.kernel.assign(w_bar)
+        self.u.assign(u)
+
+
+class MinibatchDiscrimination(layers.Layer):
+    """Minibatch discrimination layer for mode collapse prevention"""
+    def __init__(self, num_kernels=100, dim_per_kernel=5):
+        super().__init__()
+        self.num_kernels = num_kernels
+        self.dim_per_kernel = dim_per_kernel
+        
+    def build(self, input_shape):
+        self.T = self.add_weight(
+            shape=[input_shape[-1], self.num_kernels * self.dim_per_kernel],
+            initializer='random_normal',
+            trainable=True,
+            name='minibatch_T'
+        )
+        
+    def call(self, inputs):
+        # Compute minibatch features
+        x = tf.matmul(inputs, self.T)
+        x = tf.reshape(x, [-1, self.num_kernels, self.dim_per_kernel])
+        
+        # Compute L1 distances between samples
+        diffs = tf.expand_dims(x, 3) - tf.expand_dims(tf.transpose(x, [1, 2, 0]), 0)
+        abs_diffs = tf.reduce_sum(tf.abs(diffs), 2)
+        
+        # Sum over features
+        minibatch_features = tf.reduce_sum(tf.exp(-abs_diffs), 2)
+        
+        return minibatch_features
--- models/generator.py
+++ models/generator.py
@@ -1,18 +1,35 @@
 import tensorflow as tf
-from tensorflow.keras import layers
+from tensorflow.keras import layers, initializers
 
 class AdaptiveInstanceNorm(layers.Layer):
     def __init__(self, epsilon=1e-5):
         super().__init__()
         self.epsilon = epsilon
         
+    def build(self, input_shape):
+        # Add learnable scale and bias parameters
+        self.gamma = self.add_weight(
+            shape=(input_shape[-1],),
+            initializer='ones',
+            name='adain_gamma'
+        )
+        self.beta = self.add_weight(
+            shape=(input_shape[-1],),
+            initializer='zeros',
+            name='adain_beta'
+        )
+        
     def call(self, x, style):
+        # Calculate mean and variance
         mean, var = tf.nn.moments(x, axes=[1, 2], keepdims=True)
         std = tf.sqrt(var + self.epsilon)
         normalized = (x - mean) / std
         
+        # Split style into mean and std components
         style_mean, style_std = tf.split(style, 2, axis=-1)
-        out = normalized * style_std + style_mean
+        
+        # Apply style with learned parameters
+        out = (normalized * (1 + self.gamma) * style_std) + (style_mean + self.beta)
         return out
 
 class ResBlock(layers.Layer):
@@ -20,41 +37,68 @@ class ResBlock(layers.Layer):
         super().__init__()
         self.upsample = upsample
         self.norm1 = AdaptiveInstanceNorm()
-        self.conv1 = layers.Conv2D(filters, 3, padding='same')
+        self.conv1 = layers.Conv2D(
+            filters, 3, padding='same',
+            kernel_initializer=initializers.RandomNormal(0, 0.02),
+            use_bias=False
+        )
         self.norm2 = AdaptiveInstanceNorm()
-        self.conv2 = layers.Conv2D(filters, 3, padding='same')
-        self.skip = layers.Conv2D(filters, 1, padding='same')
+        self.conv2 = layers.Conv2D(
+            filters, 3, padding='same',
+            kernel_initializer=initializers.RandomNormal(0, 0.02),
+            use_bias=False
+        )
+        self.skip = layers.Conv2D(
+            filters, 1, padding='same',
+            kernel_initializer=initializers.RandomNormal(0, 0.02),
+            use_bias=False
+        )
         
     def call(self, inputs):
         x, style = inputs
+        
+        # First normalization and activation
         x = self.norm1(x, style)
-        x = tf.nn.relu(x)
+        x = tf.nn.leaky_relu(x, alpha=0.2)
+        
+        # Upsample if needed
         if self.upsample:
-            x = tf.image.resize(x, [x.shape[1]*2, x.shape[2]*2])
+            x = tf.image.resize(x, [x.shape[1]*2, x.shape[2]*2], method='nearest')
+        
+        # First convolution
         x = self.conv1(x)
         
+        # Second normalization and activation
         x = self.norm2(x, style)
-        x = tf.nn.relu(x)
+        x = tf.nn.leaky_relu(x, alpha=0.2)
+        
+        # Second convolution
         x = self.conv2(x)
         
+        # Skip connection
         if self.upsample:
-            skip = tf.image.resize(inputs[0], [x.shape[1], x.shape[2]])
-            skip = self.skip(skip)
+            skip = tf.image.resize(inputs[0], [x.shape[1], x.shape[2]], method='nearest')
         else:
-            skip = self.skip(inputs[0])
-            
-        return x + skip
+            skip = inputs[0]
+        skip = self.skip(skip)
+        
+        return x + skip * 0.1  # Weighted residual connection
 
 class StarGenerator(tf.keras.Model):
     def __init__(self, config):
         super().__init__()
         self.config = config
         
-        # Initial layers
-        self.downsample = tf.keras.Sequential([
-            layers.Conv2D(64, 7, padding='same'),
+        # Initial layers with reflection padding
+        self.initial_conv = tf.keras.Sequential([
+            layers.Lambda(lambda x: tf.pad(x, [[0,0],[3,3],[3,3],[0,0]], 'REFLECT')),
+            layers.Conv2D(
+                64, 7, padding='valid',
+                kernel_initializer=initializers.RandomNormal(0, 0.02),
+                use_bias=False
+            ),
             AdaptiveInstanceNorm(),
-            layers.ReLU()
+            layers.LeakyReLU(alpha=0.2)
         ])
         
         # Downsample blocks
@@ -64,7 +108,7 @@ class StarGenerator(tf.keras.Model):
             ResBlock(512)
         ]
         
-        # Bottleneck
+        # Bottleneck blocks
         self.bottleneck = tf.keras.Sequential([
             ResBlock(512),
             ResBlock(512),
@@ -78,30 +122,47 @@ class StarGenerator(tf.keras.Model):
             ResBlock(64, upsample=True)
         ]
         
-        # Final layers
-        self.final_conv = layers.Conv2D(3, 7, padding='same', activation='tanh')
+        # Final output layers
+        self.final_conv = tf.keras.Sequential([
+            layers.Lambda(lambda x: tf.pad(x, [[0,0],[3,3],[3,3],[0,0]], 'REFLECT')),
+            layers.Conv2D(
+                3, 7, padding='valid', activation='tanh',
+                kernel_initializer=initializers.RandomNormal(0, 0.02)
+            )
+        ])
+        
+        # Style processing
+        self.style_dense = layers.Dense(512 * 2)  # For style modulation
         
     def call(self, inputs, training=None):
         x, style = inputs
-        styles = tf.split(style, len(self.down_blocks) + len(self.bottleneck.layers) + len(self.up_blocks), axis=1)
+        
+        # Process style vector
+        style = self.style_dense(style)
+        styles = tf.split(
+            style, 
+            num_or_size_splits=len(self.down_blocks) + len(self.bottleneck.layers) + len(self.up_blocks),
+            axis=-1
+        )
         style_idx = 0
         
-        # Downsample
-        x = self.downsample(x)
+        # Initial processing
+        x = self.initial_conv(x)
+        
+        # Downsample path
         for block in self.down_blocks:
             x = block([x, styles[style_idx]])
             style_idx += 1
             
-        # Bottleneck
+        # Bottleneck path
         for block in self.bottleneck.layers:
             x = block([x, styles[style_idx]])
             style_idx += 1
             
-        # Upsample
+        # Upsample path
         for block in self.up_blocks:
             x = block([x, styles[style_idx]])
             style_idx += 1
             
         # Final output
-        x = self.final_conv(x)
-        return x
+        return self.final_conv(x)
